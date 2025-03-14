4263f5409e6b83a5a146ee96141c726a7a3e7b90
Bodi Medhat <podimedhat@hotmail.com>
2025-03-15 00:57:08 +0200

    gitsigns keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,11 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Gitsigns
+-- Gitsigns
+vim.keymap.set("n", "[h", ":Gitsigns prev_hunk<CR>", { silent = true })
+vim.keymap.set("n", "]h", ":Gitsigns next_hunk<CR>", { silent = true })
+vim.keymap.set("n", "gph", ":Gitsigns preview_hunk<CR>", { silent = true })
+vim.keymap.set({ "n", "v" }, "gsh", ":Gitsigns stage_hunk<CR>", { silent = true })
+vim.keymap.set({ "n", "v" }, "grh", ":Gitsigns reset_hunk<CR>", { silent = true })
