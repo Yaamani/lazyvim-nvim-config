676cb061a32d5008d79c4045c2fa08ba39f31c8f
Bodi Medhat <podimedhat@hotmail.com>
2025-03-17 02:25:09 +0200

    vscode theme modification

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,6 +1,31 @@
 return {
-  { "Mofiqul/vscode.nvim" },
-
+  {
+    "Mofiqul/vscode.nvim",
+    config = function()
+      local vsc = require("vscode.colors").get_colors()
+      require("vscode").setup({
+        -- transparent = true,
+        group_overrides = {
+          -- this supports the same val table as vim.api.nvim_set_hl
+          -- use colors from this colorscheme by requiring vscode.colors!
+          -- ['@constant.builtin'] = { fg = vsc.vscBlue, bg = vsc.vscNone },
+          -- ['@variable.builtin'] = { fg = vsc.vscBlue, bg = vsc.vscNone },
+          -- ['Type'] = { fg = vsc.vscBlueGreen, bg = vsc.vscNone },
+          -- ['Typedef'] = { fg = vsc.vscBlueGreen, bg = vsc.vscNone },
+          -- ['@type'] = { fg = vsc.vscBlueGreen, bg = vsc.vscNone },
+          -- ['@type.builtin'] = { fg = vsc.vscBlueGreen, bg = vsc.vscNone },
+          -- ['@type.qualifier'] = { fg = vsc.vscBlueGreen, bg = vsc.vscNone },
+          ["DiagnosticUnnecessary"] = { fg = "#888888", bg = vsc.vscNone, undercurl = true, sp = vsc.vscBlue },
+          ["SpellBad"] = { undercurl = true, sp = vsc.vscBlueGreen },
+          ["SpellCap"] = { undercurl = true, sp = vsc.vscGray },
+          ["SpellRare"] = { undercurl = true, sp = vsc.vscGray },
+          ["SpellLocal"] = { undercurl = true, sp = vsc.vscGray },
+          ["NeoTreeDotfile"] = { fg = "#626262", bg = vsc.vscNone },
+          ["NeoTreeDimText"] = { fg = "#626262", bg = vsc.vscNone },
+        },
+      })
+    end,
+  },
   {
     "LazyVim/LazyVim",
     opts = {
