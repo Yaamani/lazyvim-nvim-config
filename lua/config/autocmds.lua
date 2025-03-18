8dbdb143923d098c7200b663107b20c29bc6eee6
Bodi Medhat <podimedhat@hotmail.com>
2025-03-19 00:17:16 +0200

    Neotree refresh

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -6,3 +6,15 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+-- Neotree refresh
+-- Neotree refresh
+local neotree_refresh_group = vim.api.nvim_create_augroup('NeoTreeRefresh', { clear = true })
+vim.api.nvim_create_autocmd({ 'FocusGained', 'TermLeave', 'TermClose', 'TabEnter' }, {
+  callback = function(ev)
+    local events = require("neo-tree.events")
+    events.fire_event(events.GIT_EVENT) -- refresh neo-tree
+  end,
+  group = neotree_refresh_group,
+  pattern = '*',
+})
