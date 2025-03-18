be77468450aab1af5ee3df686c6fe35579b62e38
Bodi Medhat <podimedhat@hotmail.com>
2025-03-19 00:38:33 +0200

    performance related autocmd

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -18,3 +18,29 @@ vim.api.nvim_create_autocmd({ 'FocusGained', 'TermLeave', 'TermClose', 'TabEnter
   group = neotree_refresh_group,
   pattern = '*',
 })
+
+-- performance related autocmd
+-- performance related autocmd
+local large_files_disable_group = vim.api.nvim_create_augroup('LargeFilesDisable', { clear = true })
+-- disable some features and plugins before loading file, this time the buffer is loaded but file is still not loaded
+vim.api.nvim_create_autocmd("BufReadPre", {
+  group = large_files_disable_group,
+  pattern = "*",
+  callback = function()
+    vim.opt.spell = false
+    require("illuminate").pause_buf()
+  end,
+})
+-- after file is loaded, determine whether to enable the disabled features/plugins
+vim.api.nvim_create_autocmd("BufReadPost", {
+  group = large_files_disable_group,
+  pattern = "*",
+  callback = function()
+    local max_filesize = 500 * 1204 -- 500 KB
+    local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(vim.api.nvim_get_current_buf()))
+    if ok and stats and stats.size < max_filesize then
+      vim.opt.spell = true
+      require("illuminate").resume_buf()
+    end
+  end,
+})
