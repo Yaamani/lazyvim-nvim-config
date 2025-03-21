6ea5a095d3a98a8594f2f2efc3e89398e4ec9656
Bodi Medhat <podimedhat@hotmail.com>
2025-03-22 00:58:58 +0200

    bufferline options

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -2,7 +2,11 @@ return {
   "akinsho/bufferline.nvim",
   opts = {
     options = {
+      truncate_names = false,
       always_show_bufferline = true,
+      show_tab_indicators = true,
+      persist_buffer_sort = true,
+      sort_by = "insert_at_end",
     },
   },
 }
