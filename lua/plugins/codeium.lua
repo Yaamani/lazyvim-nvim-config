385113bbb84c9f21501ec096f26bc59b05977b00
Mahmoud Yamani <mahmoudmedhat2011@gmail.com>
2025-03-25 00:09:18 +0200

    codeium keymaps

--- lua/plugins/codeium.lua
+++ lua/plugins/codeium.lua
@@ -4,5 +4,10 @@ return {
     require("codeium.util").get_newline = function()
       return "\n"
     end
+    opts.virtual_text.key_bindings = {
+      accept = "<M-k>",
+      accept_word = "<M-l>",
+      accept_line = "<M-j>",
+    }
   end,
 }
