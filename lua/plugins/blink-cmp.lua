548abe5b362737a0c4725b60cdaba44409463834
Mahmoud Yamani <mahmoudmedhat2011@gmail.com>
2025-07-18 19:36:28 +0300

    rounded borders

---
+++ lua/plugins/blink-cmp.lua
@@ -0,0 +1,18 @@
+return {
+  "Saghen/blink.cmp",
+  opts = {
+    completion = {
+      -- ghost_text = { enabled = false },
+      menu = {
+        -- auto_show = true
+        border = "rounded"
+      },
+      -- documentation = {
+      --   border = "single",
+      -- },
+    },
+    -- signature = {
+    --   border = "single",
+    -- }
+  }
+}
