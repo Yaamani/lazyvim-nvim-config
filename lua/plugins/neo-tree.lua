63fcbc99340af33f6c5a88f8519a5cf32fc49be7
Mahmoud Yamani <mahmoudmedhat2011@gmail.com>
2026-08-24 15:38:58 +0300

    feat: relative line numbering in neo-tree

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,6 +1,15 @@
 return {
   "nvim-neo-tree/neo-tree.nvim",
   opts = {
+    event_handlers = {
+      {
+        event = "neo_tree_buffer_enter",
+        handler = function()
+          vim.opt_local.number = true
+          vim.opt_local.relativenumber = true
+        end,
+      },
+    },
     filesystem = {
       follow_current_file = {
         enabled = false
